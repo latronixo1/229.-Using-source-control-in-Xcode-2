7b41f376c970d1fe8305711b2b3f9ef2ddfed50d
latronixo1 <latronixo@gmail.com>
2023-02-25 17:05:03 +0300

    Initial Commit

---
+++ 229. Using source control in Xcode 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  229. Using source control in Xcode 2
+//
+//  Created by MacBook on 25.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 229. Using source control in Xcode 2Tests/_29__Using_source_control_in_Xcode_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _29__Using_source_control_in_Xcode_2Tests.swift
+//  229. Using source control in Xcode 2Tests
+//
+//  Created by MacBook on 25.02.2023.
+//
+
+import XCTest
+@testable import _29__Using_source_control_in_Xcode_2
+
+final class _29__Using_source_control_in_Xcode_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
